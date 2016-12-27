c1a0fbc482e45c5cf259c1150de9546dcd85505c
Arfian Adam <arfianadam@yahoo.com>
2016-12-28 02:43:13 +0700

    starting from scratch

--- src/routes.js
+++ src/routes.js
@@ -1,32 +1,11 @@
 import React from 'react';
 import { IndexRoute, Route } from 'react-router';
-import { isLoaded as isAuthLoaded, load as loadAuth } from 'redux/modules/auth';
 import { App, Home, NotFound } from 'containers';
 
 // eslint-disable-next-line import/no-dynamic-require
 if (typeof System.import === 'undefined') System.import = module => Promise.resolve(require(module));
 
 export default store => {
-  const loadAuthIfNeeded = cb => {
-    if (!isAuthLoaded(store.getState())) {
-      return store.dispatch(loadAuth()).then(() => cb());
-    }
-    return cb();
-  };
-  const checkUser = (cond, replace, cb) => {
-    const { auth: { user } } = store.getState();
-    if (!cond(user)) replace('/');
-    cb();
-  };
-
-  const requireNotLogged = (nextState, replace, cb) => {
-    const cond = user => !user;
-    loadAuthIfNeeded(() => checkUser(cond, replace, cb));
-  };
-  const requireLogin = (nextState, replace, cb) => {
-    const cond = user => !!user;
-    loadAuthIfNeeded(() => checkUser(cond, replace, cb));
-  };
 
   /**
    * Please keep routes in alphabetical order
@@ -36,23 +15,8 @@ export default store => {
       {/* Home (main) route */}
       <IndexRoute component={Home} />
 
-      {/* Routes requiring login */}
-      <Route onEnter={requireLogin}>
-        <Route path="loginSuccess" getComponent={() => System.import('./containers/LoginSuccess/LoginSuccess')} />
-        <Route path="chatFeathers" getComponent={() => System.import('./containers/ChatFeathers/ChatFeathers')} />
-      </Route>
-
-      {/* Routes disallow login */}
-      <Route onEnter={requireNotLogged}>
-        <Route path="register" getComponent={() => System.import('./containers/Register/Register')} />
-      </Route>
-
       {/* Routes */}
-      <Route path="login" getComponent={() => System.import('./containers/Login/Login')} />
-      <Route path="about" getComponent={() => System.import('./containers/About/About')} />
-      <Route path="survey" getComponent={() => System.import('./containers/Survey/Survey')} />
-      <Route path="widgets" getComponent={() => System.import('./containers/Widgets/Widgets')} />
-      <Route path="chat" getComponent={() => System.import('./containers/Chat/Chat')} />
+      {/* <Route path="login" getComponent={() => System.import('./containers/Login/Login')} /> */}
 
       {/* Catch all route */}
       <Route path="*" component={NotFound} status={404} />

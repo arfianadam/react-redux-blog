e1cdf9107cc5bd7179558f0a55b659ecb7103580
Arfian Adam <arfianadam@yahoo.com>
2016-12-29 16:31:13 +0700

    added new post component

---
+++ src/containers/New/New.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './New.scss';
+
+class New extends React.Component {
+
+  render() {
+    return (
+      <div className={styles.New}>
+        {/* Your code here */}
+      </div>
+    );
+  }
+}
+
+export default New;
